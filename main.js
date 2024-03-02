708fee488a32647737820af6a30b3bcd1f8d4b3b
bttodd11 <bttodd@icloud.com>
2024-03-01 18:06:40 -0600

    Working on rendeing 3d text

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
-
 import * as THREE from 'three';
-
+import { CSS3DRenderer } from 'three/addons/renderers/CSS3DRenderer.js';
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const geometry = new THREE.BoxGeometry( .5, .5, .5 );
-const material = new THREE.MeshBasicMaterial( { color: '#FFC107' } );
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth /window.innerHeight, 0.1, 1000 );
+camera.position.z = 45
+camera.position.x = 3
+camera.position.y = 20
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.shadowMap.enabled = true
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.target = new THREE.Vector3(0, 0, -40);
+controls.update();
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(200, 200), new THREE.MeshPhongMaterial({color: blue}))
+
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 2.5;
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.z = 5;
+
 document.body.appendChild( renderer.domElement );
 
 function animate() {
 	requestAnimationFrame( animate );
-    cube.rotation.x += 0.002;
+    cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
